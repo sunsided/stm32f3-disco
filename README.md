090670ac071ebb3b139ebd41bab382e00a35e1d1
Markus Mayer <development@widemeadows.de>
2015-02-20 23:45:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# copycat - an STM32F3-Discovery example project
+
+This is an example project for getting used to the STM32F3-Discovery board and STM32F303VCT6 MCU. 
---
+++ src/main.cpp
@@ -0,0 +1,109 @@
+#include <stm32f30x_gpio.h>
+#include <stm32f30x_rcc.h>
+#include <stm32f30x_tim.h>
+#include <stm32f30x_misc.h>
+
+/**
+ * @brief Initializes the LEDs.
+ *
+ * This function initializes the LEDs used in this example.
+ */
+void InitializeLEDs()
+{
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
+
+    GPIO_InitTypeDef gpioStructure;
+    gpioStructure.GPIO_Pin = GPIO_Pin_8;
+    gpioStructure.GPIO_Mode = GPIO_Mode_OUT;
+    gpioStructure.GPIO_OType = GPIO_OType_PP;
+    gpioStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    gpioStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOE, &gpioStructure);
+
+    GPIO_WriteBit(GPIOE, GPIO_Pin_8, Bit_RESET);
+}
+
+/**
+ * @brief Initializes the timer.
+ *
+ * This function initializes the TIM2 timer.
+ */
+void InitializeTimer()
+{
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+    TIM_TimeBaseInitTypeDef timerInitStructure;
+    timerInitStructure.TIM_Prescaler = 40000;
+    timerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    timerInitStructure.TIM_Period = 500;
+    timerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    timerInitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM2, &timerInitStructure);
+    TIM_Cmd(TIM2, ENABLE);
+
+    // allow timer interrupt
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+}
+
+/**
+ * @brief Enables the timer interrupt.
+ *
+ * This function enables the TIM2 timer interrupt.
+ */
+void EnableTimerInterrupt()
+{
+    NVIC_InitTypeDef nvicStructure;
+    nvicStructure.NVIC_IRQChannel = TIM2_IRQn;
+    nvicStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    nvicStructure.NVIC_IRQChannelSubPriority = 1;
+    nvicStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&nvicStructure);
+}
+
+/**
+* @brief  Toggles the specified GPIO pins.
+* @param  GPIOx: where x can be (A..I) to select the GPIO peripheral.
+* @param  GPIO_Pin: Specifies the pins to be toggled.
+*/
+void GPIO_ToggleBits(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+{
+    /* Check the parameters */
+    assert_param(IS_GPIO_ALL_PERIPH(GPIOx));
+
+    GPIOx->ODR ^= GPIO_Pin;
+}
+
+
+/**
+ * @brief Main entry point.
+ * @param argc argument count when semihosting
+ * @param argv argument vector when semihosting
+ * @return arbitrary exit code, should be unused as this function should never return.
+ */
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char* argv[])
+{
+    InitializeLEDs();
+    InitializeTimer();
+    EnableTimerInterrupt();
+
+    for (;;)
+    {
+    }
+
+	// yup
+	return 0;
+}
+
+/**
+ * @brief TIM2 Timer Interrupt Handler
+ */
+extern "C" void TIM2_IRQHandler()
+{
+    // check the interrupt status;
+    // if the interrupt is SET, toggle the LED
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+    {
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+        GPIO_ToggleBits(GPIOE, GPIO_Pin_8);
+    }
+}
