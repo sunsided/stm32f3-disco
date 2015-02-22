9d6f0182ac89229bc7fba9135e084943b0c41406
Markus Mayer <development@widemeadows.de>
2015-02-22 05:16:24 +0100

    updated documentation and style

--- system/src/newlib/_exit.c
+++ system/src/newlib/_exit.c
@@ -1,7 +1,8 @@
-//
-// This file is part of the µOS++ III distribution.
-// Copyright (c) 2014 Liviu Ionescu.
-//
+/**
+ * @file _exit.c
+ * This file is part of the µOS++ III distribution.
+ * Copyright (c) 2014 Liviu Ionescu.
+ */
 
 // ----------------------------------------------------------------------------
 
@@ -10,48 +11,50 @@
 
 // ----------------------------------------------------------------------------
 
-extern void
-__attribute__((noreturn))
-__reset_hardware(void);
+#if !defined(DEBUG)
+extern void __attribute__((noreturn)) __reset_hardware(void);
+#endif
 
 // ----------------------------------------------------------------------------
 
 // Forward declaration
 
-void
-_exit(int code);
+void _exit(int code);
 
 // ----------------------------------------------------------------------------
 
-// On Release, call the hardware reset procedure.
-// On Debug we just enter an infinite loop, to be used as landmark when halting
-// the debugger.
-//
-// It can be redefined in the application, if more functionality
-// is required.
-
-void
-__attribute__((weak))
-_exit(int code __attribute__((unused)))
+/**
+ * On Release, call the hardware reset procedure.
+ * On Debug we just enter an infinite loop, to be used as landmark when halting
+ * the debugger.
+ *
+ * It can be redefined in the application, if more functionality
+ * is required.
+ *
+ * @callgraph
+ * @callergraph
+ */
+void __attribute__((weak)) _exit(int code __attribute__((unused)))
 {
 #if !defined(DEBUG)
-  __reset_hardware();
+	__reset_hardware();
 #endif
 
-  // TODO: write on trace
-  while (1)
-    ;
+	// TODO: write on trace
+	for (;;) {}
 }
 
 // ----------------------------------------------------------------------------
 
-void
-__attribute__((weak,noreturn))
-abort(void)
+/**
+ * @callgraph
+ * @callergraph
+ */
+void __attribute__((weak,noreturn)) abort(void)
 {
-  trace_puts("abort(), exiting...");
+	trace_puts("abort(), exiting...");
 
-  _exit(1);
+	_exit(1);
 }
 
 // ----------------------------------------------------------------------------
